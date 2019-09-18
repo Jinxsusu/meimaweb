03ca2dde19f70b3f861561d01bece1d57540461a
Jinx <Jinx@qq.com>
2019-09-19 00:34:02 +0800

    登录表单验证

--- src/main.js
+++ src/main.js
@@ -3,7 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
-import { ValidationProvider, extend } from 'vee-validate'
+/* 下面的这种加载方式没有 携带任何一种 校验规则 */
+// import { ValidationProvider, extend } from 'vee-validate'
+
+// 您可以使用应用中占用空间较大的完整包来导入所有规则：
+// 如果您需要使用 其内置的验证规则 ,则使用这种方式来配置
+// import { ValidationProvider, extend } from 'vee-validate/dist/vee-validate.full'
+// 下面的中方式也能导入 所有的验证 规则
+// 然后 语言环境也设置了
+// import { extend } from 'vee-validate'
+// ValidationObserver 这个组件可以同时校验多个字段 也就是用于表单的整体验证
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
+import * as rules from 'vee-validate/dist/rules' // 所有的验证 规则
+import zhCN from 'vee-validate/dist/locale/zh_CN' // 配置的语言包
+
 import {
   Button,
   NavBar,
@@ -20,15 +33,40 @@ Vue
   .use(Button)
   .use(NavBar)
   .use(Toast)
-  // 配置表单验证插件
-  // 自定义添加验证规则 extend 扩展功能 (函数)
-extend('secret', {
-  validate: value => value === 'example',
-  message: 'This is not the magic word'
-})
-// Register it globally 注册了一个全局组件
-Vue.component('ValidationProvider', ValidationProvider)
 
+for (let rule in rules) {
+  extend(rule, {
+    ...rules[rule], // add the rule
+    message: zhCN.messages[rule] // add its message
+  })
+}
+
+// 配置表单验证插件
+// 自定义添加验证规则 extend 扩展功能 (函数)
+// extend('secret', {
+//   // value就是你输入以后要校验的数据
+//   validate: value => value === 'example',
+//   // validate (value) {
+//   //   console.log('插件进行验证', value)
+//   //   return value === 'example'
+//   // },
+//   // 给出一个错误提示
+//   message: 'This is not the magic word'
+// })
+// Register it globally 注册了一个全局组件
+
+// 自定义设置手机号的校验规则
+// 一个原则 如果有内置的规则 执行内置的
+// 内置的没有的话再自定义的
+extend('phone', {
+  validate (value) {
+    // 传递过来一个参数
+    return /^1[3456789]\d{9}$/.test(value)
+  },
+  message: '必须是手机号'
+})
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,57 @@
 <template>
   <div class="login">
+    <!-- validationProvider 是验证插件提供的一个全局组件
+rules="secret" 配置验证规则
+v-slot={errors} 获取验证相关的结果参数
+errors[0] 用来获取验证 的错误消息
+    name 是校验插件的组件的属性 自定义提示字段的名称 xx是必须的-->
+    <!-- <validationProvider rules="required"  name="手机号" v-slot="{ errors }">
+      <input v-model="user.mobile" type="text" />
+      <span>{{ errors[0] }}</span>
+    </validationProvider>-->
+
     <van-nav-bar title="登录" />
-    <van-cell-group>
-      <!-- v-model 绑定 组件内的数据 -->
-      <van-field v-model="user.mobile" required clearable label="手机号" placeholder="请输入手机号" />
-      <van-field v-model="user.code" type="password" label="验证码" placeholder="请输入验证码" required />
-    </van-cell-group>
+    <!-- ValidationObserver这个组件能 校验多个字段
+    这个组件会渲染成一个from 表单 tag 设置成了from
+    可以通过ref 调用这个组件的方法 :validate 来记性js验证
+    验证通过提交表单 验证失败 不提交表单-->
+    <ValidationObserver tag="form" ref="loginForm">
+      <!-- 需要验证的字段用 ValidationProvider 包裹起来
+        在其上面配置对应的验证规则等信息 就是设置 name rules 等标签属性
+          ValidationProvider这个组件只能验证一个字段
+      这个组件会渲染成一个div  因为tag 设置成了div-->
+      <validationProvider tag="div" rules="required|phone" name="手机号" v-slot="{ errors }">
+        <!-- | 后面加上我们的自定义的校验规则 -->
+        <!-- v-model 绑定 组件内的数据 -->
+        <van-field
+          :error-message="errors[0]"
+          v-model="user.mobile"
+          required
+          clearable
+          label="手机号"
+          placeholder="请输入手机号"
+        />
+      </validationProvider>
+      <validationProvider tag="div" rules="required" name="验证码" v-slot="{ errors }">
+        <van-field
+          :error-message="errors[0]"
+          v-model="user.code"
+          type="password"
+          label="验证码"
+          placeholder="请输入验证码"
+          required
+        />
+        <!-- error-message这个是vant组件中设置的 提示错误信息的属性
+        我们把它跟 ValidationProvider中的错误信息的提示绑定 v-slot="{ errors }"
+        当然上下两个errors 不是一样的.-->
+        <!-- errors是组件内置的一个数据模型，用来存储和处理错误信息。
+        默认情况下，错误包实例将注入组件 errors 名称下的计算属性中，
+        可以对其进行自定义以避免与其他 库/组件 发送冲突。 -->
+      </validationProvider>
+    </ValidationObserver>
     <div class="btn">
       <!-- 给登录按钮注册事件 -->
-      <van-button
-      @click="onLogin"
-      type="info"
-      :loading="isloading">登录</van-button>
+      <van-button @click="onLogin" type="info" :loading="isloading">登录</van-button>
     </div>
   </div>
 </template>
@@ -31,14 +71,23 @@ export default {
     }
   },
   methods: {
-    async  onLogin () {
-      this.isloading = true
+    async onLogin () {
       // 使用try 和catch 处理异步函数的错误
       try {
+        // 进行表单验证可以通过ref 调用这个组件的方法 :validate 来记性js验证
+        // 这个是一个异步的 获取到一个结果 保存到一个常量中
+        const isValid = await this.$refs.loginForm.validate()
+        // 进行一个判断 如果验证失败的话 什么的都不做  直接return
+        // 阻止代码往下执行
+        if (!isValid) {
+          return
+        }
+        // 验证通过提交表单
+        this.isloading = true
         const { data } = await login(this.user)
         // 在 login 函数中 传入对应的参数
-        this.$toast.success('登录成功')
         console.log(data)
+        this.$toast.success('登录成功')
       } catch (err) {
         // 给出错误提示
         if (err.response && err.response.status === 400) {
@@ -50,7 +99,6 @@ export default {
       // 无论登录成功还是失败 都要停止 loading 给用户返回结果
       this.isloading = false
     }
-
   }
 }
 </script>
