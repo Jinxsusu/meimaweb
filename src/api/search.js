30352b98101f01027b157aca3cce554f8d37e785
Jinx <Jinx@qq.com>
2019-09-25 22:46:20 +0800

    搜索结果方法的封装 调用方法 获取文章列表数据并渲染

--- src/api/search.js
+++ src/api/search.js
@@ -1,10 +1,27 @@
 import request from '@/utils/request'
+// 获取联想建议
 export const getSuggestions = q => {
   return request({
     method: 'GET',
     url: '/app/v1_0/suggestion',
     params: {
       q
+    }
+  })
+}
+// 获取搜索结果
+export const getSearchResult = ({
+  page,
+  perPage,
+  q
+}) => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/search',
+    params: {
+      page, // 页数，不传默认为1
+      per_page: perPage, // 每页数量，不传每页数量由后端决定
+      q // 搜索关键词
     }
   })
 }
--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -2,7 +2,8 @@
   <div class="search-result">
     <!--$router.back() 后退到上一级路由  -->
     <van-nav-bar
-     fixed title="xxx的搜索结果"
+     fixed
+     :title="`${$route.params.q}的搜索结果`"
      left-arrow
      @click-left="$router.back()" />
     <!-- 搜索结果文章列表 -->
@@ -12,38 +13,47 @@
      finished-text="没有更多了"
      @load="onLoad">
       <van-cell
-       v-for="item in list"
-       :key="item"
-       :title="item" />
+       v-for="article in list"
+       :key="article.art_id.toString()"
+       :title="article.title" />
     </van-list>
   </div>
 </template>
 
 <script>
+import { getSearchResult } from '@/api/search'
 export default {
   name: 'SearchResult',
   data () {
     return {
       list: [],
       loading: false,
-      finished: false
+      finished: false,
+      page: 1// 设置默认文章展示列表 页码为1
     }
   },
   methods: {
-    onLoad () {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
+    async onLoad () {
+    // 1.请求
+      const { data } = await getSearchResult({
+        page: this.page,
+        per_page: 10,
+        q: this.$route.params.q
+      })
+      // 2.将请求结果保存到当前组件的list中
+      const results = data.data.results
+      // console.log(data)
+      this.list.push(...results)
+      // 3.关闭loading
+      this.loading = false
+      // 4.判断是否已全部结束
+      if (results.length) {
+        // 如果有,更新页码
+        this.page++
+      } else {
+        // 如果已结束,则将finished 设置为true ,不再调用onLoad
+        this.finished = true
+      }
     }
   }
 }
